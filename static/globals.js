2cae42d191dfbf94a8d300cdd3b7e5ba804dcaf1
MarcerCyoon <4866635+MarcerCyoon@users.noreply.github.com>
2020-03-29 22:55:12 +0900

    Added motive page

--- static/globals.js
+++ static/globals.js
@@ -18,6 +18,16 @@ var interactionTwoArray = [
   "+ and + play card games together.",
   "+ surprises + with a present."
 ];
+
+var motiveArray = [
+  "Monokuma threatens to reveal your darkest secrets if nobody is killed in the next 24 hours.",
+  "Monokuma threatens to kill a person close to you that has been taken hostage if nobody is killed in the next 24 hours.",
+  "Monokuma offers 1,000,000 dollars to whoever successfully graduates.",
+  "Monokuma shows everyone a personal video that horrifies each and every one of them.",
+  "Monokuma begins to play insanely loud and annoying music. He will only turn it off if someone is murdered.",
+  "Monokuma murmurs that there is a traitor among you!"
+];
+
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
