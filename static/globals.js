378ff4bf17818c36996a66732d1ff0881a59b3ab
MarcerCyoon <4866635+MarcerCyoon@users.noreply.github.com>
2020-03-29 21:24:42 +0900

    created globals file

---
+++ static/globals.js
@@ -0,0 +1,20 @@
+// + denotes character name placeholder
+
+var interactionOneArray = [
+  "+ spends time along in their room.",
+  "+ eats some donuts.",
+  "+ does their laundry.",
+  "+ cries in their room.",
+  "+ spends the entire day sleeping.",
+  "+ takes a walk through the garden.",
+  "+ reads some books."
+];
+
+var interactionTwoArray = [
+  "+ and + go swimming together.",
+  "+ and + open up to each other.",
+  "+ and + take a bath together.",
+  "+ and + gossips about the fellow students.",
+  "+ and + play card games together.",
+  "+ surprises + with a present."
+];
